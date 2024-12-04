c899308a5a178683ff2c6f6c79ad914d928c4cc1
Aslan Nurlybekov <nurlybekov.aslan@yandex.kz>
2024-12-05 01:04:33 +0300

    email, uuid, ip

--- go.mod
+++ go.mod
@@ -2,7 +2,9 @@ module medods-test
 
 go 1.20
 
+require github.com/golang-jwt/jwt v3.2.2+incompatible
+
 require (
-	github.com/golang-jwt/jwt v3.2.2+incompatible
-	// github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
+	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,36 +2,73 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
+	"strings"
 	"time"
 
-	"github.com/golang-jwt/jwt"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 )
 
-type UserInfo struct {
-	Guid  string
-	Email string
-	Ip    string
+// type UserInfo struct {
+// 	Guid  string
+// 	Email string
+// 	Ip    string
+// }
+
+// type Claims struct {
+// 	*jwt.StandardClaims
+// 	UserInfo
+// }
+
+var secretKey = []byte("secret")
+
+func generateUUID() string {
+	uuidWithHyphen := uuid.New()
+	return uuidWithHyphen.String()
 }
 
-type Claims struct {
-	*jwt.StandardClaims
-	UserInfo
-}
+func getIP(r *http.Request) (string, error) {
+	//Get IP from the X-REAL-IP header
+	ip := r.Header.Get("X-REAL-IP")
+	netIP := net.ParseIP(ip)
+	if netIP != nil {
+		return ip, nil
+	}
 
-const secretKey = "secret"
+	//Get IP from X-FORWARDED-FOR header
+	ips := r.Header.Get("X-FORWARDED-FOR")
+	splitIps := strings.Split(ips, ",")
+	for _, ip := range splitIps {
+		netIP := net.ParseIP(ip)
+		if netIP != nil {
+			return ip, nil
+		}
+	}
+
+	//Get IP from RemoteAddr
+	ip, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		return "", err
+	}
+	netIP = net.ParseIP(ip)
+	if netIP != nil {
+		return ip, nil
+	}
+	return "", fmt.Errorf("No valid ip found")
+}
 
 func createToken(email string) (string, error) {
 	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"sub": email,
-		//"guid": guid
-		//"ip": deviceIp
 		"exp": time.Now().Add(time.Hour).Unix(),
 		"iat": time.Now().Unix(),
 	})
 
 	tokenString, err := claims.SignedString(secretKey)
 	if err != nil {
+		fmt.Printf("%s", err)
 		return "", err
 	}
 
@@ -39,18 +76,23 @@ func createToken(email string) (string, error) {
 	return tokenString, err
 }
 
-// func checkJwt(w http.ResponseWriter, r *http.Request) {
-// 	w.Write([]byte("You have accessed a protected endpoint"))
-// }
-
 func main() {
 	http.HandleFunc("/auth", func(w http.ResponseWriter, r *http.Request) {
 		email := r.FormValue("email")
-		password := r.FormValue("password")
+		//password := r.FormValue("password")
 
-		fmt.Printf("%s\n", email)
+		uuidClient := generateUUID()
 
-		if email == "email" && password == "password" {
+		ip, err := getIP(r)
+		if err != nil {
+			w.WriteHeader(400)
+			w.Write([]byte("No valid ip"))
+			return
+		}
+
+		fmt.Printf("%s\n%s\n%s\n", email, ip, uuidClient)
+
+		if email == "email" {
 			token, err := createToken(email)
 			if err != nil {
 				http.Error(w, "Failed", http.StatusInternalServerError)
@@ -69,6 +111,33 @@ func main() {
 	})
 
 	http.HandleFunc("/protected", func(w http.ResponseWriter, r *http.Request) {
+		clientToken := r.Header.Get("Authorization")
+
+		if clientToken == "" {
+			fmt.Fprintf(w, "Authorization Token is required")
+			return
+		}
+
+		claims := jwt.MapClaims{}
+		tokenString := strings.Join(strings.Split(clientToken, "Bearer "), "")
+
+		// Parse the claims
+		_, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+			return secretKey, nil
+		})
+
+		if err != nil {
+			if err == jwt.ErrSignatureInvalid {
+				fmt.Printf("%s\n", err)
+				return
+			}
+			fmt.Printf("%s\n", err)
+			return
+		}
+
+		for key, val := range claims {
+			fmt.Printf("Key: %v, value: %v\n", key, val)
+		}
 
 	})
 
